89f39e583a12aea7f4530d3ee7fe88d7a53d5060
SHOAIB-PC\SHOAIB <andrew.sim84@gmail.com>
2018-12-18 21:09:53 +0100

    HackerRankTasks

---
+++ mainer.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+
+
+if __name__ == '__main__':
+    d = deque()
+    N = int(input())
+    for i in range(N):
+        lInput = input().split()
+        if (len(lInput) == 1):
+            eval('d.' + lInput[0] + '()')
+        else:
+            eval('d.' + lInput[0] + '(' + lInput[1] + ')')
+
+    print(d)
